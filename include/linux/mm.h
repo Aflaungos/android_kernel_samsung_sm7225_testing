5c6f126bc4477a1ad3e1ef6efed618aa8c222034
Minchan Kim <minchan@google.com>
2019-03-11 11:42:36 +0900

    Revert "mm: make pte_unmap_same compatible with SPF"
    This reverts commit 976415c621472112cf886ae11adaba779d40ccea.
    
    Bug: 128240262
    Change-Id: Ifdf06b5394bcfb111468fd7f220434a343fb48e8
    Signed-off-by: Minchan Kim <minchan@google.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1395,8 +1395,6 @@ static inline void clear_page_pfmemalloc(struct page *page)
 	page->index = 0;
 }
 
-#define VM_FAULT_PTNOTSAME 0x4000	/* Page table entries have changed */
-
 /*
  * Can be called by the pagefault handler when it gets a VM_FAULT_OOM.
  */
