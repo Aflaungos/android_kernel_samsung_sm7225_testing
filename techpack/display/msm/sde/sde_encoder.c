618acec54c08697b57fe4753bdb9edd925b35c51
Midas Chien <midaschieh@google.com>
2020-04-22 16:29:32 +0800

    techpack: drm/msm/sde: add sysfs node for trigger wake up early
    Add sysfs node on crtc to trigger sde wake up early.
    The command to trigger wake up early give in shell:
    
    echo 1 > /sys/class/drm/sde-crtc-*/early_wakeup
    
    Bug: 150196517
    Test: trigger wake up and switch display power state
    Test: testInflatingEmojiListViewFling
    Test: testBrowseContentScroll
    Change-Id: I96e6ad73e83e8a9e3e5e65e064c92f8e6a6d63f8
    Signed-off-by: Midas Chien <midaschieh@google.com>

--- techpack/display/msm/sde/sde_encoder.c
+++ techpack/display/msm/sde/sde_encoder.c
@@ -6548,3 +6548,26 @@ struct sde_encoder_phys *ss_get_encoder_phys(struct drm_encoder *drm_enc, int ds
 }
 #endif
 
+void sde_encoder_trigger_early_wakeup(struct drm_encoder *drm_enc)
+{
+	struct sde_encoder_virt *sde_enc = NULL;
+	struct msm_drm_private *priv = NULL;
+
+	priv = drm_enc->dev->dev_private;
+	sde_enc = to_sde_encoder_virt(drm_enc);
+	if (!sde_enc->crtc || (sde_enc->crtc->index
+			>= ARRAY_SIZE(priv->disp_thread))) {
+		SDE_DEBUG_ENC(sde_enc,
+			"invalid cached CRTC: %d or crtc index: %d\n",
+			sde_enc->crtc == NULL,
+			sde_enc->crtc ? sde_enc->crtc->index : -EINVAL);
+		return;
+	}
+
+	if (sde_enc->rc_state == SDE_ENC_RC_STATE_IDLE) {
+		SDE_ATRACE_BEGIN("sde_encoder_resource_control");
+		sde_encoder_resource_control(drm_enc,
+					SDE_ENC_RC_EVENT_EARLY_WAKEUP);
+		SDE_ATRACE_END("sde_encoder_resource_control");
+	}
+}
