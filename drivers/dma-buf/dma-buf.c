f2b949e1aa3435d16bf950c680f93d75e4b3900d
Charan Teja Reddy <charante@codeaurora.org>
2020-09-18 16:02:31 +0530

    UPSTREAM: dmabuf: fix NULL pointer dereference in dma_buf_release()
    commit 19a508bd1ad8e444de86873bf2f2b2ab8edd6552 upstream.
    
    NULL pointer dereference is observed while exporting the dmabuf but
    failed to allocate the 'struct file' which results into the dropping of
    the allocated dentry corresponding to this file in the dmabuf fs, which
    is ending up in dma_buf_release() and accessing the uninitialzed
    dentry->d_fsdata.
    
    Call stack on 5.4 is below:
     dma_buf_release+0x2c/0x254 drivers/dma-buf/dma-buf.c:88
     __dentry_kill+0x294/0x31c fs/dcache.c:584
     dentry_kill fs/dcache.c:673 [inline]
     dput+0x250/0x380 fs/dcache.c:859
     path_put+0x24/0x40 fs/namei.c:485
     alloc_file_pseudo+0x1a4/0x200 fs/file_table.c:235
     dma_buf_getfile drivers/dma-buf/dma-buf.c:473 [inline]
     dma_buf_export+0x25c/0x3ec drivers/dma-buf/dma-buf.c:585
    
    Fix this by checking for the valid pointer in the dentry->d_fsdata.
    
    Fixes: 4ab59c3c638c ("dma-buf: Move dma_buf_release() from fops to dentry_ops")
    Cc: <stable@vger.kernel.org> [5.7+]
    Change-Id: I187196ba9d9c7e177d8dba5240661227e23d189b
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/391319/
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -111,6 +111,8 @@ static void dma_buf_release(struct dentry *dentry)
 	int dtor_ret = 0;
 
 	dmabuf = dentry->d_fsdata;
+	if (unlikely(!dmabuf))
+		return;
 
 	spin_lock(&dentry->d_lock);
 	dentry->d_fsdata = NULL;
