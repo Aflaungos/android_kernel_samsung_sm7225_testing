ad2fd532a6af3e3d809b0c4f95678f1ff8ab3fa6
Paul Lawrence <paullawrence@google.com>
2022-02-25 23:35:30 +0000

    UPSTREAM: ANDROID: fuse-bpf: Update attributes on file write
    Bug: 221093504
    Test: atest android.hardware.cts.CameraTest#testJpegExif
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: Ideb4f4d95e60594aed9000df4c21bacfaeac3a55

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1324,8 +1324,8 @@ static inline void fuse_freezer_count(void) {}
 int fuse_readdir(struct file *file, struct dir_context *ctx);
 
 /* passthrough.c */
-int fuse_passthrough_open(struct fuse_dev *fud,
-			  struct fuse_passthrough_out *pto);
+void fuse_copyattr(struct file *dst_file, struct file *src_file);
+int fuse_passthrough_open(struct fuse_dev *fud, u32 lower_fd);
 int fuse_passthrough_setup(struct fuse_conn *fc, struct fuse_file *ff,
 			   struct fuse_open_out *openarg);
 void fuse_passthrough_release(struct fuse_passthrough *passthrough);
