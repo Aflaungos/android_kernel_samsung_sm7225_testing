a98e244b7d5f450135a41e961575cd0447d96744
Rameshkumar Sundaram <quic_ramess@quicinc.com>
2022-04-11 14:37:51 +0530

    UPSTREAM: cfg80211: hold bss_lock while updating nontrans_list
    Synchronize additions to nontrans_list of transmitting BSS with
    bss_lock to avoid races. Also when cfg80211_add_nontrans_list() fails
    __cfg80211_unlink_bss() needs bss_lock to be held (has lockdep assert
    on bss_lock). So protect the whole block with bss_lock to avoid
    races and warnings. Found during code review.
    
    Bug: 254441685
    Fixes: 0b8fb8235be8 ("cfg80211: Parsing of Multiple BSSID information in scanning")
    Signed-off-by: Rameshkumar Sundaram <quic_ramess@quicinc.com>
    Link: https://lore.kernel.org/r/1649668071-9370-1-git-send-email-quic_ramess@quicinc.com
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    (cherry picked from commit a5199b5626cd6913cf8776a835bc63d40e0686ad)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I06b4d5f79fa97b11b2aff61e4f060e8eaca719b2

--- net/wireless/scan.c
+++ net/wireless/scan.c
@@ -1396,6 +1396,7 @@ cfg80211_inform_single_bss_data(struct wiphy *wiphy,
 		/* this is a nontransmitting bss, we need to add it to
 		 * transmitting bss' list if it is not there
 		 */
+		spin_lock_bh(&rdev->bss_lock);
 		if (cfg80211_add_nontrans_list(non_tx_data->tx_bss,
 					       &res->pub)) {
 			if (__cfg80211_unlink_bss(rdev, res)) {
@@ -1406,7 +1407,7 @@ cfg80211_inform_single_bss_data(struct wiphy *wiphy,
 		spin_unlock_bh(&rdev->bss_lock);
 
 		if (!res)
-			return NULL;		
+			return NULL;
 	}
 
 	trace_cfg80211_return_bss(&res->pub);
