7ceaed034d18a264dccfe769de6bb6aa0117a4db
Vincent Guittot <vincent.guittot@linaro.org>
2023-11-22 14:39:03 +0100

    sched/cpufreq: Rework schedutil governor performance estimation
    The current method to take into account uclamp hints when estimating the
    target frequency can end in a situation where the selected target
    frequency is finally higher than uclamp hints, whereas there are no real
    needs. Such cases mainly happen because we are currently mixing the
    traditional scheduler utilization signal with the uclamp performance
    hints. By adding these 2 metrics, we loose an important information when
    it comes to select the target frequency, and we have to make some
    assumptions which can't fit all cases.
    
    Rework the interface between the scheduler and schedutil governor in order
    to propagate all information down to the cpufreq governor.
    
    effective_cpu_util() interface changes and now returns the actual
    utilization of the CPU with 2 optional inputs:
    
    - The minimum performance for this CPU; typically the capacity to handle
      the deadline task and the interrupt pressure. But also uclamp_min
      request when available.
    
    - The maximum targeting performance for this CPU which reflects the
      maximum level that we would like to not exceed. By default it will be
      the CPU capacity but can be reduced because of some performance hints
      set with uclamp. The value can be lower than actual utilization and/or
      min performance level.
    
    A new sugov_effective_cpu_perf() interface is also available to compute
    the final performance level that is targeted for the CPU, after applying
    some cpufreq headroom and taking into account all inputs.
    
    With these 2 functions, schedutil is now able to decide when it must go
    above uclamp hints. It now also has a generic way to get the min
    performance level.
    
    The dependency between energy model and cpufreq governor and its headroom
    policy doesn't exist anymore.
    
    eenv_pd_max_util() asks schedutil for the targeted performance after
    applying the impact of the waking task.
    
    [ mingo: Refined the changelog & C comments. ]
    
    Change-Id: If151dc717eaf2d587ece0ee91673e627febdffc5
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Rafael J. Wysocki <rafael@kernel.org>
    Link: https://lore.kernel.org/r/20231122133904.446032-2-vincent.guittot@linaro.org
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7926,6 +7926,7 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 	unsigned long cpu_cap = arch_scale_cpu_capacity(cpumask_first(pd_mask));
 #endif
 	unsigned long max_util = 0, sum_util = 0;
+	unsigned long min, max;
 	int cpu;
 	unsigned long cpu_util;
 
@@ -7944,7 +7945,6 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 		sum_util += cpu_util;
 #else
 		unsigned long util_cfs = cpu_util_next(cpu, p, dst_cpu);
-		struct task_struct *tsk = cpu == dst_cpu ? p : NULL;
 
 		/*
 		 * Busy time computation: utilization clamping is not
@@ -7952,7 +7952,7 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 		 * is already enough to scale the EM reported power
 		 * consumption at the (eventually clamped) cpu_capacity.
 		 */
-		sum_util += schedutil_cpu_util(cpu, util_cfs, ENERGY_UTIL, NULL);
+		sum_util += schedutil_cpu_util(cpu, util_cfs, NULL, NULL);
 
 		/*
 		 * Performance domain frequency: utilization clamping
@@ -7961,7 +7961,15 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 		 * NOTE: in case RT tasks are running, by default the
 		 * FREQUENCY_UTIL's utilization can be max OPP.
 		 */
-		cpu_util = schedutil_cpu_util(cpu, util_cfs, FREQUENCY_UTIL, tsk);
+		cpu_util = schedutil_cpu_util(cpu, util_cfs, &min, &max);
+			/* Task's uclamp can modify min and max value */
+			if (uclamp_is_used()) {
+				min = max(min, uclamp_eff_value(p, UCLAMP_MIN));
+
+				max = max(max, uclamp_eff_value(p, UCLAMP_MAX));
+			}
+
+			cpu_util = sugov_effective_cpu_perf(cpu, cpu_util, min, max);
 #endif
 		max_util = max(max_util, cpu_util);
 	}
