55a89b4a2e50b81488337af5788652d52caff51c
Aflaungos <vimavick@gmail.com>
2024-12-02 10:33:53 -0300

    script: Improve coding

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ ON_BLUE=`echo -e "\033[44m"`
 RED=`echo -e "\033[1;31m"`
 BLUE=`echo -e "\033[1;34m"`
 GREEN=`echo -e "\033[1;32m"`
-STD=`echo -e "\033[0m"`		# Clear colour
+STD=`echo -e "\033[0m"`
 
 
 ####################### Devices List #########################
