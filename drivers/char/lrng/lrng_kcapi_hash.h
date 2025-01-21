92628c7cd9ce2f9501e6432d0c2e3a977d8bd3eb
Park Ju Hyung <qkrwngud825@gmail.com>
2025-01-21 12:41:41 -0300

    lrng: merge v43
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/char/lrng/lrng_kcapi_hash.h
+++ drivers/char/lrng/lrng_kcapi_hash.h
@@ -2,9 +2,12 @@
 /*
  * Copyright (C) 2020 - 2021, Stephan Mueller <smueller@chronox.de>
  */
+
 #ifndef _LRNG_KCAPI_HASH_H
 #define _LRNG_KCAPI_HASH_H
+
 #include <linux/module.h>
+
 void *lrng_kcapi_hash_alloc(const char *name);
 u32 lrng_kcapi_hash_digestsize(void *hash);
 void lrng_kcapi_hash_dealloc(void *hash);
@@ -13,4 +16,5 @@ int lrng_kcapi_hash_update(struct shash_desc *shash, const u8 *inbuf,
 			   u32 inbuflen);
 int lrng_kcapi_hash_final(struct shash_desc *shash, u8 *digest);
 void lrng_kcapi_hash_zero(struct shash_desc *shash);
+
 #endif /* _LRNG_KCAPI_HASH_H */
