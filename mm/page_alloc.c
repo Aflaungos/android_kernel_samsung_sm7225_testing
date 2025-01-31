384941d4fb1054b15e7dc18b0ed5bf8ac49243d8
Aflaungos <vimavick@gmail.com>
2025-01-31 12:14:00 -0300

    defconfig debloat and fix compilation

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7636,7 +7636,6 @@ early_param("movablecore", cmdline_parse_movablecore);
 
 void adjust_managed_page_count(struct page *page, long count)
 {
-	spin_lock(&managed_page_count_lock);
 	page_zone(page)->managed_pages += count;
 	totalram_pages += count;
 #ifdef CONFIG_HIGHMEM
