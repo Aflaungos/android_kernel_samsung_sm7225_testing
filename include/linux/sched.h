db694ea1bbd71659949fbd707956b2638dd023db
Aflaungos <vimavick@gmail.com>
2025-05-20 19:42:51 -0300

    include/linux: sched.h: Fix duplicate definition

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1508,7 +1508,7 @@ struct task_struct {
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
-#if defined(CONFIG_KSU_SUSFS)
+#if defined(CONFIG_KSU_SUSFS) && !defined(ANDROID_KABI_RESERVE)
 	u64 susfs_task_state;
 #endif
 #if defined(CONFIG_KSU_SUSFS) && !defined(ANDROID_KABI_RESERVE)
