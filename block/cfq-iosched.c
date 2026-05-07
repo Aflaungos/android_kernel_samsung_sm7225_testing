50a1b94e0aceb7684fb7fc79f3a18de202c12185
Panchajanya1999 <panchajanya@azure-dev.live>
2026-05-07 00:51:35 -0300

    cfq-iosched: Implement Read-only macro for entry attribute
    It is necessary to make some entries Read-Only since Android post-boot
    will change these values.
    
    Change-Id: Ie29fabc36fe843f336f86c5d2b019fcb1a2763ef
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -4890,7 +4890,7 @@ USEC_STORE_FUNCTION(cfq_target_latency_us_store, &cfqd->cfq_target_latency, 1, U
         __ATTR(name, 0444, cfq_##name##_show, NULL)
 
 static struct elv_fs_entry cfq_attrs[] = {
-	CFQ_ATTR(quantum),
+	CFQ_ATTR_RO(quantum),
 	CFQ_ATTR(fifo_expire_sync),
 	CFQ_ATTR(fifo_expire_async),
 	CFQ_ATTR(back_seek_max),
