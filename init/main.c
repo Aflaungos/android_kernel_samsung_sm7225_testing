6c2280926319934016cb51ffca0352cffad3bb93
Aflaungos <vimavick@gmail.com>
2026-07-16 19:40:45 -0300

    Revert "!fixup: "dma_buf: use kmem_cache pool for struct dma_buf_attachment""
    This reverts commit de7ad4412bce6898e4a3170ba0218fbe2ed4a870.
    
    Change-Id: Idad6c2f7dbdf16463301ff35a21e86411c14e21b

--- init/main.c
+++ init/main.c
@@ -598,7 +598,6 @@ static void __init mm_init(void)
 	pti_init();
 }
 
-void __init init_dma_buf_kmem_pool(void);
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
@@ -799,7 +798,6 @@ asmlinkage __visible void __init start_kernel(void)
 	cgroup_init();
 	taskstats_init_early();
 	delayacct_init();
-	init_dma_buf_kmem_pool();
 
 
 	acpi_subsystem_init();
