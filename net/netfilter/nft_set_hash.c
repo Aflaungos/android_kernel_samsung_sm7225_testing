5f0179982b2420d8593f38583a1bd6559da89068
Aflaungos <vimavick@gmail.com>
2026-07-18 12:47:25 -0300

    Revert "netfilter: nft_set_hash: skip duplicated elements pending gc run"
    This reverts commit 002d757b7a3986ab808de1d71a83489ee4e716a1.
    
    Change-Id: Ifaa22070cdad3a55683c385fb47efc4be9b51407

--- net/netfilter/nft_set_hash.c
+++ net/netfilter/nft_set_hash.c
@@ -30,12 +30,10 @@ extern unsigned int nf_tables_net_id;
 struct nft_rhash {
 	struct rhashtable		ht;
 	struct delayed_work		gc_work;
-	u32				wq_gc_seq;
 };
 
 struct nft_rhash_elem {
 	struct rhash_head		node;
-	u32				wq_gc_seq;
 	struct nft_set_ext		ext;
 };
 
@@ -319,10 +317,6 @@ static void nft_rhash_gc(struct work_struct *work)
 	if (!gc)
 		goto done;
 
-	/* Elements never collected use a zero gc worker sequence number. */
-	if (unlikely(++priv->wq_gc_seq == 0))
-		priv->wq_gc_seq++;
-
 	err = rhashtable_walk_init(&priv->ht, &hti, GFP_KERNEL);
 	if (err) {
 		nft_trans_gc_destroy(gc);
@@ -345,14 +339,6 @@ static void nft_rhash_gc(struct work_struct *work)
 			goto try_later;
 		}
 
-		/* rhashtable walk is unstable, already seen in this gc run?
-		 * Then, skip this element. In case of (unlikely) sequence
-		 * wraparound and stale element wq_gc_seq, next gc run will
-		 * just find this expired element.
-		 */
-		if (he->wq_gc_seq == priv->wq_gc_seq)
-			continue;
-
 		if (nft_set_elem_is_dead(&he->ext))
 			goto dead_elem;
 
@@ -372,8 +358,6 @@ dead_elem:
 		if (!gc)
 			goto try_later;
 
-		/* annotate gc sequence for this attempt. */
-		he->wq_gc_seq = priv->wq_gc_seq;
 		nft_trans_gc_elem_add(gc, he);
 	}
 try_later:
