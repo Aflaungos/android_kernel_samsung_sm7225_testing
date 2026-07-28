1269e8b7e609587c248ad2154362fff86ed869d5
Aflaungos <vimavick@gmail.com>
2026-07-27 23:48:42 -0300

    security/integrity: Remove more FIVE stuff
    Change-Id: Ide1822777f8d0aa2b1f16f249a6d65efdfaadb76

--- security/integrity/integrity.h
+++ security/integrity/integrity.h
@@ -16,16 +16,6 @@
 #include <crypto/sha.h>
 #include <linux/key.h>
 #include <linux/audit.h>
-#include <crypto/hash_info.h>
-
-enum five_file_integrity {
-	FIVE_FILE_UNKNOWN,
-	FIVE_FILE_FAIL,
-	FIVE_FILE_RSA,
-	FIVE_FILE_DMVERITY,
-	FIVE_FILE_FSVERITY,
-	FIVE_FILE_HMAC
-};
 
 /* iint action cache flags */
 #define IMA_MEASURE		0x00000001
@@ -64,9 +54,6 @@ enum five_file_integrity {
 #define IMA_READ_APPRAISED	0x00080000
 #define IMA_CREDS_APPRAISE	0x00100000
 #define IMA_CREDS_APPRAISED	0x00200000
-
-#define FIVE_DMVERITY_PROTECTED	0x00040000
-#define FIVE_TRUSTED_FILE	0x00080000
 
 #define IMA_APPRAISE_SUBMASK	(IMA_FILE_APPRAISE | IMA_MMAP_APPRAISE | \
 				 IMA_BPRM_APPRAISE | IMA_READ_APPRAISE | \
@@ -158,8 +145,7 @@ int integrity_kernel_read(struct file *file, loff_t offset,
 #define INTEGRITY_KEYRING_EVM		0
 #define INTEGRITY_KEYRING_IMA		1
 #define INTEGRITY_KEYRING_MODULE	2
-#define INTEGRITY_KEYRING_FIVE		3
-#define INTEGRITY_KEYRING_MAX		4
+#define INTEGRITY_KEYRING_MAX		3
 
 extern struct dentry *integrity_dir;
 
@@ -170,8 +156,6 @@ int integrity_digsig_verify(const unsigned int id, const char *sig, int siglen,
 
 int __init integrity_init_keyring(const unsigned int id);
 int __init integrity_load_x509(const unsigned int id, const char *path);
-int __init integrity_load_x509_from_mem(const unsigned int id,
-					const char *data, size_t size);
 #else
 
 static inline int integrity_digsig_verify(const unsigned int id,
