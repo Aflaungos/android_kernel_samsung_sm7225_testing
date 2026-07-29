89da8964c0c67add2f691cb90acaeafcef2e3d83
Aflaungos <vimavick@gmail.com>
2026-07-29 14:07:33 -0300

    Revert "UPSTREAM: net: switch sock_setbindtodevice to sockptr_t"
    This reverts commit 30ac5d5e3969dc0b4f43bd781f93fa9254bac2df.

--- net/core/sock.c
+++ net/core/sock.c
@@ -602,7 +602,8 @@ int sock_bindtoindex(struct sock *sk, int ifindex, bool lock_sk)
 }
 EXPORT_SYMBOL(sock_bindtoindex);
 
-static int sock_setbindtodevice(struct sock *sk, sockptr_t optval, int optlen)
+static int sock_setbindtodevice(struct sock *sk, char __user *optval,
+				int optlen)
 {
 	int ret = -ENOPROTOOPT;
 #ifdef CONFIG_NETDEVICES
@@ -629,7 +630,7 @@ static int sock_setbindtodevice(struct sock *sk, sockptr_t optval, int optlen)
 	memset(devname, 0, sizeof(devname));
 
 	ret = -EFAULT;
-	if (copy_from_sockptr(devname, optval, optlen))
+	if (copy_from_user(devname, optval, optlen))
 		goto out;
 
 	index = 0;
@@ -821,7 +822,7 @@ int sock_setsockopt(struct socket *sock, int level, int optname,
 	 */
 
 	if (optname == SO_BINDTODEVICE)
-		return sock_setbindtodevice(sk, USER_SOCKPTR(optval), optlen);
+		return sock_setbindtodevice(sk, optval, optlen);
 
 	if (optlen < sizeof(int))
 		return -EINVAL;
